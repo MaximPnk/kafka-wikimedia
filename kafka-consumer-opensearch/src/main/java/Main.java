a0054cd6a916860d381eae378f02aeb3fbb3db52
Maxim <mx.pnk@ya.ru>
2022-03-18 00:18:13 +0300

    create openSearch producer

--- kafka-consumer-opensearch/src/main/java/Main.java
+++ kafka-consumer-opensearch/src/main/java/Main.java
@@ -1,2 +1,31 @@
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.indices.CreateIndexRequest;
+import org.opensearch.client.indices.GetIndexRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws IOException {
+        String openSearchHost = "";
+
+        try (InputStream input = OpenSearchClient.class.getResourceAsStream("opensearch.properties")) {
+            Properties properties = new Properties();
+            properties.load(input);
+            openSearchHost = "http://" + properties.getProperty("opensearch.host");
+        } catch (IOException e) {
+            log.error("Error while reading props", e);
+        }
+
+        OpenSearchClient client = new OpenSearchClient(openSearchHost);
+        client.createIndex();
+    }
+
 }
---
+++ kafka-consumer-opensearch/src/main/java/OpenSearchClient.java
@@ -0,0 +1,64 @@
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.indices.CreateIndexRequest;
+import org.opensearch.client.indices.GetIndexRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class OpenSearchClient {
+
+    private static final Logger log = LoggerFactory.getLogger(OpenSearchClient.class);
+
+    private final RestHighLevelClient client;
+    private final String host;
+
+    public OpenSearchClient(String host) {
+        this.host = host;
+        URI uri = URI.create(host);
+        String userInfo = uri.getUserInfo();
+
+        if (userInfo == null) {
+            client = new RestHighLevelClient(RestClient.builder(new HttpHost(uri.getHost(), uri.getPort(), "http")));
+        } else {
+            String[] auth = userInfo.split(":");
+            CredentialsProvider provider = new BasicCredentialsProvider();
+            provider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
+            client = new RestHighLevelClient(RestClient.builder(new HttpHost(uri.getHost(), uri.getPort(), uri.getScheme()))
+                    .setHttpClientConfigCallback(httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(provider)
+                            .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())));
+        }
+    }
+
+    public RestHighLevelClient getClient() {
+        return client;
+    }
+
+    public void createIndex() {
+        RestHighLevelClient client = new OpenSearchClient(host).getClient();
+
+        try (client) {
+            GetIndexRequest getIndexRequest = new GetIndexRequest("wikimedia");
+
+            if (!client.indices().exists(getIndexRequest, RequestOptions.DEFAULT)) {
+                CreateIndexRequest createIndexRequest = new CreateIndexRequest("wikimedia");
+                client.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+                log.info("The wikimedia index has been created");
+            } else {
+                log.info("The wikimedia index already exists");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
