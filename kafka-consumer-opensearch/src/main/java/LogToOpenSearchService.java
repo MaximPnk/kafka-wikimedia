79096edf17eb46f0419621a8a61148d3f60fdcfa
Maxim <mx.pnk@ya.ru>
2022-03-18 13:20:23 +0300

    fix elastic duplicates

--- kafka-consumer-opensearch/src/main/java/LogToOpenSearchService.java
+++ kafka-consumer-opensearch/src/main/java/LogToOpenSearchService.java
@@ -1,3 +1,5 @@
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.opensearch.action.index.IndexRequest;
@@ -28,14 +30,26 @@ public class LogToOpenSearchService {
         while (true) {
             ConsumerRecords<String, String> records = kafkaWikimediaConsumer.getConsumer().poll(Duration.ofMillis(100));
             for (ConsumerRecord<String, String> r : records) {
+//                String id = r.topic() + "_" + r.partition() + "_" + r.offset();
+                String id = extractId(r.value());
                 try {
-                    IndexRequest indexRequest = new IndexRequest("wikimedia").source(r.value(), XContentType.JSON);
-                    System.out.println(r.value());
+                    IndexRequest indexRequest = new IndexRequest("wikimedia").source(r.value(), XContentType.JSON).id(id);
                     IndexResponse indexResponse = openSearchClient.getClient().index(indexRequest, RequestOptions.DEFAULT);
                     log.info(indexResponse.getId());
-                } catch (Exception ignored) {}
+                } catch (Exception e) {
+                    log.error("Fail to send log to elastic", e);
+                }
             }
         }
+    }
+
+    private String extractId(String value) {
+        return JsonParser.parseString(value)
+                .getAsJsonObject()
+                .get("meta")
+                .getAsJsonObject()
+                .get("id")
+                .getAsString();
     }
 
 }
